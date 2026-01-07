2b9a5140f0281c0fa8989d13b18ebabd37e01f1a
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-07 21:32:31 +0530

    setup app.js

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+export default app
