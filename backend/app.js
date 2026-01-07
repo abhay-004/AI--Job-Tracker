cdd3c5c8231c1ac7f523f4156d79cd33dbb04c9b
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-08 01:48:13 +0530

    add user api endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 
 //middlewares
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cookieParser());
 
+//api endpoints
 
+app.use("/api/v1/user", userRouter);
 
-export default app
+export default app;
