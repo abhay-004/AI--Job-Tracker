35bebe3ba2ea1b7114eed2bfa2fcd25c0783ebe3
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-08 15:11:22 +0530

    create profile route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,6 +4,8 @@ import {
   logOutUser,
   registerUser,
 } from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+import { getUserProfile } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
@@ -12,5 +14,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/logout", logOutUser);
+userRouter.get("/profile", authMiddleware, getUserProfile);
 
 export default userRouter;
