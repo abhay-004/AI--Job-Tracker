218944d5f54ed6efa52d3435c509fc37372bab95
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-10 19:20:19 +0530

    add resumeText

--- backend/controllers/resume.controller.js
+++ backend/controllers/resume.controller.js
@@ -2,6 +2,7 @@
 
 import UserModel from "../models/User.model.js";
 import cloudinary from "../utils/cloudinary.js";
+import extractFromPdf from "../utils/pdfParser.js";
 
 export const uploadResume = async (req, res) => {
   try {
@@ -11,19 +12,26 @@ export const uploadResume = async (req, res) => {
         .json({ success: false, message: "NO file uploaded" });
     }
 
+    //extract text
+    const resumeText = await extractFromPdf(req.file.path);
+
+    //upload to cloudinary
     const result = await cloudinary.uploader.upload(req.file.path, {
       resource_type: "raw",
       folder: "resumes",
     });
 
+    //save resume + text
     await UserModel.findByIdAndUpdate(req.user._id, {
       resume: result.secure_url,
+      resumeText,
     });
 
     return res.status(200).json({
       success: true,
       message: "Resume uploaded successfully",
       resumeUrl: result.secure_url,
+      preview: resumeText.slice(0, 300),
     });
   } catch (error) {
     console.error("UPLOAD ERROR:", error);
