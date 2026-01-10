0cad54e7484abc8707abd3cfa49777841fb5ca0e
Abhay Chauhan <chauhanabhay88688@gmail.com>
2026-01-10 15:06:29 +0530

    create resume controller

---
+++ backend/controllers/resume.controller.js
@@ -0,0 +1,33 @@
+//upload resume
+
+import UserModel from "../models/User.model.js";
+import cloudinary from "../utils/cloudinary.js";
+
+export const uploadResume = async (req, res) => {
+  try {
+    console.log("file", req.file);
+    if (!req.file) {
+      return res
+        .status(400)
+        .json({ success: false, message: "NO file uploaded" });
+    }
+
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      resource_type: "raw",
+      folder: "resumes",
+    });
+
+    await UserModel.findByIdAndUpdate(req.user._id, {
+      resume: result.secure_url,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Resume uploaded successfully",
+      resumeUrl: result.secure_url,
+    });
+  } catch (error) {
+    console.error("UPLOAD ERROR:", error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
